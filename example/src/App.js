30394d1efc1991335f7a51ee96b5151abf6a4344
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2021-05-26 16:07:29 +0430

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-switch-tis'
+import 'react-switch-tis/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
